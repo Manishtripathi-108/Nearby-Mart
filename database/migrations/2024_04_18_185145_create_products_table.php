49d5dd2bbe645dcf032545aebf9d01d1abd47aa8
Manishtripathi-108 <manish82644@gmail.com>
2024-04-19 02:24:47 +0530

    Add database migration files for locations, user details, stores, carts, business hours, orders, payments, and order items

---
+++ database/migrations/2024_04_18_185145_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('store_id')->constrained()->cascadeOnDelete();
+            $table->string('photo_main')->default('no-photo.jpg');
+            $table->string('photo_1')->default('no-photo.jpg');
+            $table->string('photo_2')->default('no-photo.jpg');
+            $table->string('name');
+            $table->tinyInteger('rating')->default(0);
+            $table->decimal('price', 10, 2);
+            $table->decimal('discount', 10, 2)->nullable();
+            $table->enum('discount_type', ['Fixed', 'Percentage'])->nullable();
+            $table->enum('availability', ['Available', 'Unavailable']);
+            $table->text('description')->nullable();
+            $table->unsignedInteger('stock');
+            $table->unsignedInteger('units_sold')->default(0);
+            $table->integer('measure');
+            $table->enum('sold_by', ['kg', 'g', 'lb', 'pcs', 'units', 'each', 'ml', 'l', 'fl oz']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
