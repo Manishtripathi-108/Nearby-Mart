2ea8bca72ab5679e3688628ec5452ad9baa58065
Manishkumar76 <mkshakya794@gmail.com>
2024-04-25 23:34:56 +0530

    welcome page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,19 @@
 
 <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    
+<body class="">
+    <div class="fixed bg-gradient-to-r from-blue-200  to-purple-200  w-full h-full text-black flex pl-20 pr-20 ">
+    <ul class="block ">
+          <li class="inline-block">
+          <div class="w-200 h-16 fixed text-blue-900 font-serif text-xl font-bold item-center hover:text-black">
+            Near-by Mart
+          </div>
+          </li>
+
+        <li>
+       </ul> 
+       <div class=" fixed bg-red-500 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+         
+      </div>
 </body>
 </html>
