acd1787aa81e0bbaa4a3da65ccd79ca8767bb94e
Manishkumar76 <mkshakya794@gmail.com>
2024-04-26 00:16:13 +0530

    welcome page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,18 +9,72 @@
 
 <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="">
-    <div class="fixed bg-gradient-to-r from-blue-200  to-purple-200  w-full h-full text-black flex pl-20 pr-20 ">
-    <ul class="block ">
-          <li class="inline-block">
-          <div class="w-200 h-16 fixed text-blue-900 font-serif text-xl font-bold item-center hover:text-black">
-            Near-by Mart
-          </div>
-          </li>
+<body class="bg-gradient-to-r from-blue-200  to-purple-200 w-full h-full">
 
-        <li>
-       </ul> 
-       <div class=" fixed bg-red-500 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <div class="fixed   w-full h-16 text-black flex pl-20 pr-20 items-center justify-end content-around md:">
+    <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3 ">
+                        @if (Route::has('login'))
+                            <nav class="-mx-3 flex flex-1 justify-end ">
+                                @auth
+                                    <a
+                                        href="{{ url('/dashboard') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Dashboard
+                                    </a>
+                                @else
+                                    <a
+                                        href="{{ route('login') }}"
+                                        class="rounded-md px-3 py-2 text-black border-2 rounded-sm ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white dark:border-white dark:border-opacity-50"
+                                    >
+                                        Log in
+                                    </a>
+
+                                    @if (Route::has('register'))
+                                        <a
+                                            href="{{ route('register') }}"
+                                            class="rounded-md px-3 py-2 text-black border-2 rounded ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >
+                                            Register
+                                        </a>
+                                    @endif
+                                @endauth
+                            </nav>
+                        @endif
+                    </header> 
+    </div>
+             <div class="h-full w-full flex flex-col justify-center items-center table-column">
+             @if (Route::has('login'))
+                            <nav class="-mx-3 flex flex-1 justify-end">
+                                @auth
+                                    <a
+                                        href="{{ url('/dashboard') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Dashboard
+                                    </a>
+                                @else
+                                    <a
+                                        href="{{ route('login') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Log in
+                                    </a>
+
+                                    @if (Route::has('register'))
+                                        <a
+                                            href="{{ route('register') }}"
+                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >
+                                            Register
+                                        </a>
+                                    @endif
+                                @endauth
+                            </nav>
+                        @endif
+             </div>
+
+       <div class=" fixed bg-red-500 top-1/2 left-1/2 w-600 h-7000 transform -translate-x-1/2 -translate-y-1/2">
          
       </div>
 </body>
