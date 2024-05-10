b7acc10acaeebcec3bb731817feb316f3310757e
Manishkumar76 <mkshakya794@gmail.com>
2024-05-11 01:11:16 +0530

    user-profile updation

--- resources/views/profile/user-profile.blade.php
+++ resources/views/profile/user-profile.blade.php
@@ -1,31 +1,144 @@
 <x-app-layout>
-    <div class="h-screen bg-gray-200  dark:bg-gray-800   flex flex-wrap items-center  justify-center  ">
-        <div class="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
-            <div class=" h-32 overflow-hidden">
-                <img class="w-full" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="" />
-            </div>
-            <div class="flex justify-center px-5  -mt-12">
-                <img class="h-32 w-32 bg-white p-2 rounded-full   " src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="" />
+    <div class="flex flex-col justify-around items-center md:flex-row-reverse ">
 
-            </div>
-            <div class=" ">
-                <div class="text-center px-14">
-                    <h2 class="text-gray-800 text-3xl font-bold">{{$user->name}}</h2>
-                    <p class="text-gray-400 mt-2 hover:text-blue-500 border-none" type="email">{{$user->email}}</p>
-                    <p class="mt-2 text-gray-500 text-sm"> </p>
-                </div>
-                <hr class="mt-6" />
-                <div class="flex  bg-gray-50 ">
-                    <div class="text-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer">
-                        <p><span class="font-semibold">Date of Birth: </span> {{$user->dob}}</p>
+        <div class="flex-1 flex-col h-auto w-full ">
+            <!--user profile-->
+            <div class="flex-1 flex-col h-auto w-full m-10 p-3  space-y-4">
+                <div class="flex justify-center items-center">
+                    <div class="w-24 h-auto m-2 p-2">
+                        <img src="" alt="user" class="h-cover w-full bg-blend-screen">
                     </div>
-                    <div class="border"></div>
-                    <div class="text-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer">
-                        <p> <span class="font-semibold">2.0 k </span> Following</p>
+                </div>
+                <div class="flex justify-center items-center">
+                    <h2 class="text-2xl font-bold">{{auth()->user()->name}}</h2>
+                </div>
+                <div class="flex justify-center items-center">
+                    <p class="text-sm">{{auth()->user()->email}}</p>
+                    <p class="text-sm">{{auth()->user()->phone}}</p>
+                </div>
+            </div>
+
+            <!--other Accounts-->
+            <div class="flex-1 flex-col justify-center items-center h-auto m-10 p-3  space-y-4 border-2 rounded-lg ">
+                <h2 class="text-blue-400 text-2xl font-bold">Other Accounts</h2>
+                <a href="/create-seller">
+                    <p class="text-gray-700">Seller Accout</p>
+                </a>
+            </div>
+
+            <!--recently viewed poducts-->
+            <div class="  flex flex-col h-auto  space-y-4">
+                <h2 class="text-2xl font-bold">Recently Viewed</h2>
+                <!--product card-slider-->
+                <div class="hidden relative flex-col md:flex flex-nowrap space-x-4 items-center justify-evenly ">
+                    <!--left slider-button-->
+                    <div class="flex absolute left-2 top-1/2 transform -translate-y-1/2 items-center">
+                        <button id="prev"
+                            class="bg-gray-800 text-white rounded-full w-8 h-8 flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M15 19l-7-7 7-7" />
+                            </svg>
                     </div>
 
-                </div>
+                    <!--right slider-button-->
+                    <div class="flex absolute right-2 top-1/2 transform -translate-y-1/2 items-center">
+                        <button id="next"
+                            class="bg-gray-800 text-white rounded-full w-8 h-8 flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M9 5l7 7-7 7" />
+                            </svg>
+                        </button>
+                    </div>
+
+                    <!--product cards-->
+                    
+
+
             </div>
         </div>
+    </div>
+
+    <div class="flex-2 flex-col h-auto w-full m-10 p-3 justify-center item-center space-y-4 md:w-1/4">
+
+        <!--Your profile-->
+        <div
+            class="flex justify-center flex-row h-30 w-full  border-2 rounded-lg items-center transition eas-in-out delay-2  hover:border-3 hover:border-blue-400">
+            <div class="w-24 h-auto m-2 p-2">
+                <img src="{{ asset('images/profile/profile-edit.jpeg') }}" alt="user"
+                    class="h-cover w-full bg-blend-screen">
+            </div>
+            <div class="flex-col justify-center items-center">
+                <h2 class="text-2xl font-bold">Edit Profile</h2>
+                <p class="text-sm">Edit your personal details</p>
+            </div>
+        </div>
+
+        <!--Your order-->
+        <div
+            class="flex justify-center flex-row h-30 w-full  border-2 rounded-lg items-center transition eas-in-out delay-2  hover:border-3 hover:border-blue-400">
+            <div class="w-24 h-auto m-2 p-2">
+                <img src="{{ asset('images/profile/delivery-box.jpeg') }}" alt="user"
+                    class="h-cover w-full bg-blend-screen">
+            </div>
+            <div class="flex-col justify-center items-center">
+                <h2 class="text-2xl font-bold">Your Orders</h2>
+                <p class="text-sm">Check your orders status</p>
+            </div>
+        </div>
+
+        <!--Your wishlist-->
+        <div
+            class="flex justify-center flex-row h-30 w-full  border-2 rounded-lg items-center transition eas-in-out delay-2  hover:border-3 hover:border-blue-400">
+            <div class="w-24 h-auto m-2 p-2">
+                <img src="{{ asset('images/profile/wish-list.jpeg') }}" alt="user"
+                    class="h-cover w-full bg-blend-screen">
+            </div>
+            <div class="flex-col justify-center items-center">
+                <h2 class="text-2xl font-bold">Your Wishlist</h2>
+                <p class="text-sm">Create / Check your wishlist</p>
+            </div>
+        </div>
+
+        <!--Your location-->
+        <div
+            class="flex justify-center flex-row h-30 w-full  border-2 rounded-lg items-center transition eas-in-out delay-2  hover:border-3 hover:border-blue-400">
+            <div class="w-24 h-auto m-2 p-2">
+                <img src="{{ asset('images/profile/location.jpeg') }}" alt="user"
+                    class="h-cover w-full bg-blend-screen">
+            </div>
+            <div class="flex-col justify-center items-center">
+                <h2 class="text-2xl font-bold">Your Address</h2>
+                <pfass="text-sm">Edit your address for orders and gifts</p>
+            </div>
+        </div>
+
+        <!--user seller accout if have-->
+        @if(Auth()->check())
+        <!-- seller account-->
+        @if(auth()->user()->role == 'Store-owner')
+        <div
+            class="flex justify-center flex-row h-30 w-full  border-2 rounded-lg items-center transition eas-in-out delay-2  hover:border-3 hover:border-blue-400">
+            <div class="w-24 h-auto m-2 p-2">
+                <img src="{{ asset('images/profile/store.jpeg') }}" alt="user" class="h-cover w-full bg-blend-screen">
+            </div>
+            <div class="flex-col justify-center items-center">
+                <h2 class="text-2xl font-bold">Your Store</h2>
+                <p class="text-sm">Add more products in your digital store</p>
+            </div>
+        </div>
+
+        @endif
+
+        @endif
+
+
+    </div>
+
+
+
     </div>
 </x-app-layout>
